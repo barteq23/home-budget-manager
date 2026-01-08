a007708884375de8667131087ebad08ec72f3f23
barteq23 <bartipas@o2.pl>
2026-01-08 19:30:05 +0100

    Update bank_api_client.py

--- bank_api_client.py
+++ bank_api_client.py
@@ -2,3 +2,9 @@ class BankApiClient:
     def fetch_transactions(self, account_id):
         # TODO: implement API call to bank
         return []
+
+# change on main branch
+class BankApiClient:
+    def fetch_transactions(self, account_id):
+        # TODO: implement API call to bank
+        return []
